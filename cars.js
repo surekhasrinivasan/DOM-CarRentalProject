2427162d54040154f8a774ee38b23ed96fc942b6
surekha <surekhasrao@gmail.com>
2017-11-17 16:22:29 +0000

    Final commit

--- cars.js
+++ cars.js
@@ -1,26 +1,35 @@
-var carsRental = { 
-    businessname: 'Enterprise Rent-A-Car',
-    econCars: 500,
-    midsizeCars: 200,
-    renter: "",
-};
-
 var rental = {
-    types: ['Economy','Midsize'],
-    econcarprice: "$200",
-    midsizecarprice: "$300"
+	businessname: 'Enterprise Car Rental',
+	rentername: "",
+};
+var carRental = {
+	EconomyCars: {
+		rentalprice: 50,
+		carsavailable: 9
+	},
+	MidsizeCars: {
+		rentalprice: 80,
+		carsavailable: 10
+	}
+};
+window.onload = function() {
+	document.getElementById("busiName").innerHTML = rental.businessname;
 };
 
-window.onload = function(){
-    document.getElementById("busiName").innerHTML = carsRental.businessname;
-};
+function displayCar(val) {
+	document.getElementById('displayInfo').innerHTML = carRental[val].carsavailable + "  " + val + " Available" + "<br>" + "$" + carRental[val].rentalprice + "  / day";
+}
 
-function validateForm() {
-    var x = document.forms["myForm"]["rentername"].value;
-    if (x == "") {
-        alert("Renter's Name must be filled out");
-        return false;
-    }
-};
-
-
+function rentFunction() {
+	var cartype = document.forms["carSelect"]['cartype'].value;
+	var renter = document.forms["carSelect"]['rentername'].value;
+	if (cartype == "none") {
+		alert("Please select a car type");
+	} else if (renter == "") {
+		alert("Please enter the Renter's name");
+	} else {
+		carRental[cartype].carsavailable--;
+		document.getElementById('displayInfo').innerHTML = "Your Rental Car is ready for pick up!";
+	}
+	return false;
+}
