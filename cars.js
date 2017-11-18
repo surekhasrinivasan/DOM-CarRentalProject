c627bdaa2551b31d11729f7250a50842e129c9ca
surekha <surekhasrao@gmail.com>
2017-11-18 18:10:09 +0000

    Made changes to cars.js

--- cars.js
+++ cars.js
@@ -1,8 +1,5 @@
-var rental = {
-	businessname: 'Enterprise Car Rental',
-	rentername: "",
-};
 var carRental = {
+	businessname: 'Enterprise Car Rental',
 	EconomyCars: {
 		rentalprice: 50,
 		carsavailable: 9
@@ -12,8 +9,12 @@ var carRental = {
 		carsavailable: 10
 	}
 };
+var rental = {
+	EconomyCars:[],
+	MidsizeCars: []
+};
 window.onload = function() {
-	document.getElementById("busiName").innerHTML = rental.businessname;
+	document.getElementById("busiName").innerHTML = carRental.businessname;
 };
 
 function displayCar(val) {
@@ -22,13 +23,19 @@ function displayCar(val) {
 
 function rentFunction() {
 	var cartype = document.forms["carSelect"]['cartype'].value;
+	console.log(cartype);
 	var renter = document.forms["carSelect"]['rentername'].value;
+	console.log(renter);
 	if (cartype == "none") {
 		alert("Please select a car type");
 	} else if (renter == "") {
 		alert("Please enter the Renter's name");
 	} else {
+		rental[cartype].push({name:renter});
 		carRental[cartype].carsavailable--;
+		if(carRental[cartype].carsavailable <= 0){
+		alert(cartype + " not available");
+	}
 		document.getElementById('displayInfo').innerHTML = "Your Rental Car is ready for pick up!";
 	}
 	return false;
